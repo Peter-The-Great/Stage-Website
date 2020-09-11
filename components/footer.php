e9ade270657353963ba1f6177066166da32b9a7d
Larsjarred9 <larskoudenburg@zeelandnet.nl>
2020-09-11 10:32:02 +0200

    [FRONT] Footer

---
+++ components/footer.php
@@ -0,0 +1,9 @@
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+					<p><u><a href="index.php">Stage Applicatie</a></u> is een Applicatie gemaakt voor studenten en leraren om stages te onderhouden.</p>
+					<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.glr.nl" target="_blank">GLR</a></p>
+				</div>
+				<hr>
+			</div>	
+		</div>
