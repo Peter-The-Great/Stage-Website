a2a6f79b9d4cab5e4606227e3d232ab433ce245e
Larsjarred9 <larskoudenburg@zeelandnet.nl>
2020-09-11 12:24:58 +0200

    [FRONT] Login Brands

--- components/login.php
+++ components/login.php
@@ -1,67 +1,62 @@
 <!-- Login Page -->
 <div class="container">
-    <div class="row mt-3">
-        <form action="php/auth.php" method="POST">
-            <div class="form-group">
-                <label for="email">E-Mail Address</label>
-                <input type="email" class="form-control" placeholder="student@glr.nl" id="email" name="email">
-            </div>
-            <div class="form-group">
-                <label for="password">Wachtwoord</label>
-                <input type="password" class="form-control" id="password" name="password">
-            </div>
-            <button type="submit" class="btn btn-danger">Inloggen</button>
-        </form>
+    <div class="row mt-4">
+        <div class="login-box shadow p-3 mb-5 mx-auto">
+            <h4 class="card-title mb-4 mt-1">Log in om verder te gaan.</h4>
+            <form action="php/auth.php" method="POST">
+                <div class="form-group">
+                    <label for="email">E-Mail adres</label>
+                    <input type="email" class="form-control" placeholder="student@glr.nl" id="email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Wachtwoord</label>
+                    <input type="password" placeholder="*****" class="form-control" id="password" name="password">
+                </div>
+                <button type="submit" class="btn btn-danger">Inloggen</button>
+            </form>
+        </div>
     </div>
-    <div class="row mt-3">
+    <div class="row mt-3 mx-auto">
         <!-- Bedrijven Logos -->
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body">
-                <img src="Deloitte.jpg" alt="" class="img-fluid">
+            <div class="card-body shadow h-100">
+                <img src="images/Deloitte.png" alt="" class="img-fluid">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body">
-                <img src="google.jpg" alt="" class="img-fluid">
+            <div class="card-body shadow h-100">
+                <img src="images/Google.png" alt="" class="img-fluid">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body">
-                <img src="Nikon.png" alt="" class="img-fluid">
+            <div class="card-body shadow h-100">
+                <img src="images/Nikon.png" alt="" class="img-fluid">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body">
-                <img src="Twitter.jpg" alt="" class="img-fluid">
+            <div class="card-body shadow h-100">
+                <img src="images/Twitter.png" alt="" class="img-fluid">
             </div>
         </div>
 
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body">
-                <img src="Unicef.png" alt="" class="img-fluid">
+            <div class="card-body shadow h-100">
+                <img src="images/Ziggo.png" alt="" class="img-fluid">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body">
-                <img src="Ziggo.png" alt="" class="img-fluid">
+            <div class="card-body shadow h-100">
+                <img src="images/Valk.png" alt="" class="img-fluid">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body">
-                <img src="HCL.jpg" alt="" class="img-fluid">
+            <div class="card-body shadow h-100">
+                <img src="images/Netflix.png" alt="" class="img-fluid">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body">
-                <img src="Deltafiber.png" alt="" class="img-fluid">
-            </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body">
-                <img src="Netflix.png" alt="" class="img-fluid">
-            </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body">
-                <img src="CitiznM.png" alt="" class="img-fluid">
+            <div class="card-body shadow h-100">
+                <img src="images/Deltafiber.png" alt="" class="img-fluid">
             </div>
         </div>
     </div>
