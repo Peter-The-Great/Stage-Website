058f55d15fe2a807f62284a372519dd76dc39950
Larsjarred9 <larskoudenburg@zeelandnet.nl>
2020-09-11 09:12:38 +0200

    [FRONT] Linked CSS / JavaScript files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Stage | Stage Portaal</title>
+</head>
+<body>
+    <?php include 'components/navigation.php' ?>
+    <?php include 'components/footer.php' ?>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+</body>
+</html>
