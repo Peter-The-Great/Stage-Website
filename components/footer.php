76c595977be6f6ff8fd15ff26a8114eb40a16a2b
Larsjarred9 <larskoudenburg@zeelandnet.nl>
2020-09-11 11:43:45 +0200

    [BUGFIX] Footer Layout / Styling

--- components/footer.php
+++ components/footer.php
@@ -1,9 +1,11 @@
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-					<p><u><a href="index.php">Stage Applicatie</a></u>Dit is een applicatie gemaakt voor studenten en leraren om stages bij te houden.</p>
-					<p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.glr.nl" target="_blank">GLR</a></p>
-				</div>
-				<hr>
-			</div>	
+		<div class="container-fluid">
+		    <div class="row">
+		        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white bg-dark">
+		            <div class="m-3">
+		                <p class="m-0 p-0"><a class="text-danger" href="index.php">Stage Applicatie</a> - Een applicatie gemaakt voor studenten en leraren om stages bij te houden.
+		                    <br>© 2020 <a class="text-danger" href="https://www.glr.nl" target="_blank">Grafisch Lyceum Rotterdam®</a>, All rights reserved.</p>
+		            </div>
+		        </div>
+		        <hr>
+		    </div>
 		</div>
