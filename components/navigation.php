aa6c7bb09479fadc2f4b45f24b44c6b0614367dd
Larsjarred9 <larskoudenburg@zeelandnet.nl>
2020-09-18 11:49:58 +0200

    [BUGFIX]

--- components/navigation.php
+++ components/navigation.php
@@ -3,14 +3,13 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mx-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="#">Home</span></a>
             </li>
             <?php
-            if (isset($_SESSION['rol']) && $_SESSION['rol'] == "student") { // If user is student
+            if (isset($_SESSION['rol']) && $_SESSION['rol'] == "student") { // If user is studentt
                 echo "<li class='nav-item'>
                 <a class='nav-link' href='stages.php'>Stages</a>
                 </li>
