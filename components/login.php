9a9956abfc79f5e0e920880218c714f67a728963
Larsjarred9 <larskoudenburg@zeelandnet.nl>
2020-09-18 08:51:25 +0200

    [BUGFIX] Images now working correctly

--- components/login.php
+++ components/login.php
@@ -2,7 +2,7 @@
 <div class="container">
     <div class="row mt-4">
         <div class="login-box shadow p-3 mb-5 mx-auto">
-            <h4 class="card-title mb-4 mt-1">Log in om verder te gaan.</h4>
+            <h4 class="card-title mb-4 mt-1">Log in om verder te gaan!</h4>
             <form action="php/auth.php" method="POST">
                 <div class="form-group">
                     <label for="email">E-Mail adres</label>
@@ -20,43 +20,43 @@
         <!-- Bedrijven Logos -->
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Deloitte.png" alt="" class="img-fluid">
+                <img src="images/Deloitte.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block pt-2">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Google.png" alt="" class="img-fluid">
+                <img src="images/Google.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Nikon.png" alt="" class="img-fluid">
+                <img src="images/Nikon.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Twitter.png" alt="" class="img-fluid">
+                <img src="images/Twitter.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block pt-3">
             </div>
         </div>
 
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Ziggo.png" alt="" class="img-fluid">
+                <img src="images/Ziggo.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Valk.png" alt="" class="img-fluid">
+                <img src="images/Valk.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block pt-3">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Netflix.png" alt="" class="img-fluid">
+                <img src="images/Videoland.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block pt-3">
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Deltafiber.png" alt="" class="img-fluid">
+                <img src="images/Deltafiber.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block">
             </div>
         </div>
     </div>
