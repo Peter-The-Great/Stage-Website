a4e50d370b6bed38997b61885b4ef4718468f16b
Larsjarred9 <larskoudenburg@zeelandnet.nl>
2020-09-18 11:35:56 +0200

    [BUGFIX]
    Scripts

--- index.php
+++ index.php
@@ -18,20 +18,16 @@ if (isset($_SESSION['rol'])) {
 <body>
     <!-- Layout -->
     <?php require 'components/navigation.php';
+    //require 'components/dashboard_student.php';
     // Serve Content
     if (!isset($_SESSION['login'])) { // If user hasn't logged
         require 'components/login.php';
-    } else if (isset($_SESSION['login'])) { // If user has logged in
-        if ($rol == "student") { // If user is student
-            require 'components/dashboard_student.php';
-        } else if ($rol == "leraar") { // If user is teacher
-            require 'components/dashboard_leraar.php';
-        }
     }
     include 'components/footer.php' ?>
+    <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/popper.min.js"></script>
-    <script src="js/jquery-3.5.1.min.js"></script>
+    
 </body>
 
 </html>
