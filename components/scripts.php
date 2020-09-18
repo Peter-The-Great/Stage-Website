af78f436e96136f356dd2c75477f3299b8690739
Peter-The-Great <56716150+Peter-The-Great@users.noreply.github.com>
2020-09-18 12:16:19 +0200

    One small change to my code

--- components/scripts.php
+++ components/scripts.php
@@ -1,5 +1,5 @@
+<svg id="fader"></svg>
 <script src="js/jquery-3.5.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/popper.min.js"></script>
 <script src="js/fade-in.js"></script>
-<!--<svg id="fader"></svg>-->
