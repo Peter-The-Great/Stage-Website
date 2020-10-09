edc340bb0d4ad5e74175f516cac420257add5e3a
Larsjarred9 <larskoudenburg@zeelandnet.nl>
2020-10-09 11:48:07 +0200

    Stage tips en bedrijven

--- components/login.php
+++ components/login.php
@@ -25,43 +25,22 @@
         <!-- Bedrijven Logos -->
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Deloitte.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block pt-2">
+                <a href="https://www.indeed.nl/"><img src="images/indeed.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block pt-2"></a>
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Google.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block">
+            <a href="https://stageplaza.nl"><img src="images/stage.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block"></a>
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Nikon.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block">
+            <a href="https://www.studentenbureau.nl/tips"><img src="images/sb.png" style="max-width: 150px; max-height: 50px;" alt="" class="img-fluid mx-auto d-block"></a>
             </div>
         </div>
         <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
             <div class="card-body shadow h-100">
-                <img src="images/Twitter.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block pt-3">
-            </div>
-        </div>
-
-        <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body shadow h-100">
-                <img src="images/Ziggo.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block">
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body shadow h-100">
-                <img src="images/Valk.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block pt-3">
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body shadow h-100">
-                <img src="images/Videoland.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block pt-3">
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4 col-lg-3 mb-3">
-            <div class="card-body shadow h-100">
-                <img src="images/Deltafiber.png" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block">
+            <a href="https://www.ictergezocht.nl"><img src="images/ict.jpg" style="max-width: 150px;" alt="" class="img-fluid mx-auto d-block pt-3"></a>
             </div>
         </div>
     </div>
